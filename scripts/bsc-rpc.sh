05f01e6bfaf8923c879b5892128a6d766aed22b5
Ubuntu <ubuntu@ip-10-0-1-79.ec2.internal>
2022-12-02 22:49:12 +0000

    Update BSC RPC node config & restart policies for containers

--- scripts/bsc-rpc.sh
+++ scripts/bsc-rpc.sh
@@ -10,10 +10,10 @@ unlock_sequences="0"
 while [ "$i" -lt ${account_cnt} ]; do
     unlock_sequences="${unlock_sequences},${i}"
     i=$(( i + 1 ))
-done 
+done
 
 geth --config ${DATA_DIR}/config.toml --datadir ${DATA_DIR} --netrestrict ${CLUSTER_CIDR} \
     --verbosity ${VERBOSE} --nousb --ethstats ${NODE_ID}:${NETSTATS_URL} \
-    -unlock ${unlock_sequences} --password /dev/null --http --http.addr=0.0.0.0 --http.vhosts=* --http.corsdomain=* --http.api=net,web3,eth,debug,txpool \
-    --ws --ws.addr=0.0.0.0 --ws.api=net,web3,eth,shh,txpool,debug --syncmode=full --gcmode=archive
+    --unlock ${unlock_sequences} --password /dev/null --http --http.addr=0.0.0.0 --http.vhosts=* --http.corsdomain=* --http.api=net,web3,eth,debug,txpool \
+    --ws --ws.addr=0.0.0.0 --ws.api=net,web3,eth,shh,txpool,debug,db,admin,miner,personal --ws.origins=* --syncmode=full --gcmode=archive --ws.port=8546
 
